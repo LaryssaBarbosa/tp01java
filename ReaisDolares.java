c30f120bcca4276daf50751072b17f826d8f2e96
Laryssa Barbosa <168648324+LaryssaBarbosa@users.noreply.github.com>
2025-03-30 10:34:57 -0300

    ReaisDolares.java

--- ReaisDolares.java
+++ ReaisDolares.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
-	public class ReaisDolares{
-        public static void main(String[] args){
-         Scanner scan = new Scanner(System.in);
-         System.out.println("Digite a quantidade de dinheiro (em dolar):");
-         double dolar = scan.nextDouble();
+public class ReaisDolares {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite a quantidade de dinheiro (em dolar):");
+        double dolar = scan.nextDouble();
  
-         double conversao = (dolar/5.76);
-        System.out.println("O valor em dolar: " + dolar +" convertido em real eh : R$:" + conversao);
-scan.close();
-         
-  
-
-   }      
+        double conversao = (dolar * 5.76);  
+        System.out.println("O valor em dólar: " + dolar + " convertido em real é: R$ " + conversao);
+        
+        scan.close();
+    }      
 }
