85361010df73f39015bf3f8606fc34fa8aac5535
Laryssa Barbosa <168648324+LaryssaBarbosa@users.noreply.github.com>
2025-03-30 10:33:10 -0300

    VolumeLivre.java

--- VolumeLivre.java
+++ VolumeLivre.java
@@ -1,31 +1,36 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
 public class VolumeLivre {
     public static void main(String[] args) {
   
         Scanner scan = new Scanner(System.in);
 
-
+     
         System.out.print("Digite a aresta do cubo : ");
         double aresta = scan.nextDouble();
 
-
+       
         System.out.print("Digite o raio da esfera : ");
         double raio = scan.nextDouble();
 
-    
-        double pi = 3.14;
+
+        if (raio > aresta) {
+            System.out.println("O raio da esfera nao pode ser maior que a aresta do cubo.");
+        } else {
+            double pi = 3.14;
+
+
+            double volumeCubo = aresta * aresta * aresta;
+
+        
+            double volumeEsfera = (4.0 / 3.0) * pi * raio * raio * raio;
 
    
-        double volumeCubo = aresta * aresta * aresta;
+            double volumeLivre = volumeCubo - volumeEsfera;
 
-    
-        double volumeEsfera = (4.0 / 3.0) * pi * raio * raio * raio;
-
-        double volumeLivre = volumeCubo - volumeEsfera;
-
-        System.out.println("O volume livre do ambiente é: " + volumeLivre);
-
+          
+            System.out.println("O volume livre do ambiente eh: " + volumeLivre);
+        }
 
         scan.close();
     }
